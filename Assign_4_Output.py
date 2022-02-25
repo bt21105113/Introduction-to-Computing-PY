898de97753ed3aa1fc1285f1c9f49725d4ec7dd2
bt21105113 <96478163+bt21105113@users.noreply.github.com>
2022-02-25 04:04:21 -0800

    Add files via upload

---
+++ Assign_4_Output.py
@@ -0,0 +1,79 @@
+Python 3.9.10 (tags/v3.9.10:f2f3f53, Jan 17 2022, 15:14:21) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+========= RESTART: C:/Users/HP/Documents/CODING/Python/Assignment_4.py =========
+Question 1
+
+Enter number of disk in tower of Hanoi:3
+
+The Disks are numbered starting from top of the tower.
+Steps to move all disks from Source Tower to Destination Tower is given below:
+
+Move disk 1 from Source Tower to Destination
+Move disk 2 from Source Tower to Auxiliary
+Move disk 1 from Destination to Auxiliary
+Move disk 3 from Source Tower to Destination
+Move disk 1 from Auxiliary to Source Tower
+Move disk 2 from Auxiliary to Destination
+Move disk 1 from Source Tower to Destination
+>>> 
+======== RESTART: C:/Users/HP/Documents/CODING/Python/Assignment_4/2.py ========
+
+[Question.2]
+
+Enter number of rows:6
+                                            1                                             
+                                           1  1                                           
+                                         1  2  1                                          
+                                        1  3  3  1                                        
+                                      1  4  6  4  1                                       
+                                    1  5  10  10  5  1                                    
+>>> 
+======== RESTART: C:/Users/HP/Documents/CODING/Python/Assignment_4/3.py ========
+Question 3
+
+Enter the first integer: 5
+Enter the second integer: 7
+Remainder is:  5
+Quotient is:  0
+One value is zero
+{9}
+{9, 10}
+{9, 10, 5}
+{9, 10, 11, 5}
+{5, 6, 9, 10, 11}
+{5, 6, 9, 10, 11}
+Immutable set:  frozenset({5, 6, 9, 10, 11})
+Largest value in the set:  11
+Hash value:  11
+
+>>> 
+======== RESTART: C:/Users/HP/Documents/CODING/Python/Assignment_4/4.py ========
+QUESTION 4
+
+Object created
+The name of the student is Anjali and SID is 21105113.
+Object destroyed
+
+>>> 
+======== RESTART: C:/Users/HP/Documents/CODING/Python/Assignment_4/5.py ========
+QUESTION_5
+
+a. The updated salary of the employee Mehak is 70000
+b. Employee Viren record deleted
+ 
+>>> 
+======== RESTART: C:/Users/HP/Documents/CODING/Python/Assignment_4/6.py ========
+QUESTION_6
+Enter the first word: Universe
+
+Enter a new meaningful word to test your friendship: Galaxy
+The letters aren't exact, friendship is fake!!
+
+Does the word makes sense?(y or n)
+y
+The friends pass the friendship test!
+
+
+
+>>> 
